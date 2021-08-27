0bce49172e7de0116786e16428ceb6c80cda4ed2
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2021-08-27 18:12:30 +0100

    Update resolveProductionUrl.js

--- resolveProductionUrl.js
+++ resolveProductionUrl.js
@@ -1,5 +1,6 @@
 const previewSecret = 'MY_SECRET'; // Copy the string you used for SANITY_PREVIEW_SECRET
-const projectUrl = 'http://localhost:3000';
+// const projectUrl = 'http://localhost:3000';
+const projectUrl = 'https://cms-sanity-app-bay.vercel.app';
 
 export default function resolveProductionUrl(document) {
   return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`;
