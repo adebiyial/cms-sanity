34eee4a93bd70e385f2600248fcecb268e983b03
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2021-08-27 18:23:42 +0100

    Update resolveProductionUrl.js

--- resolveProductionUrl.js
+++ resolveProductionUrl.js
@@ -1,8 +1,9 @@
 const previewSecret = 'MY_SECRET'; // Copy the string you used for SANITY_PREVIEW_SECRET
 // const projectUrl = 'http://localhost:3000';
-// const projectUrl = 'https://cms-sanity-app-bay.vercel.app'; - vercel
+// Vercel
+const projectUrl = 'https://cms-sanity-app-bay.vercel.app';
 // Layer0
-const projectUrl = 'https://adebiyi-adedotun-ne-cms-sanity-app-default.layer0.link';
+// const projectUrl = 'https://adebiyi-adedotun-ne-cms-sanity-app-default.layer0.link';
 
 export default function resolveProductionUrl(document) {
   return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`;
